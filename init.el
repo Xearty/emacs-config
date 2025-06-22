3ca902c0b75d7cf998ea3ebab75bd7401d8452a0
Martin Ivanov <xeartybg@gmail.com>
2025-06-22 13:49:55 +0300

    Install `marginalia`

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit vertico kanagawa-themes evil)))
+ '(package-selected-packages '(marginalia magit vertico kanagawa-themes evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
