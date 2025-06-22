80d7e0c8a0d9c1b3a5ef841ec2da0ee4e90887ce
Martin Ivanov <xeartybg@gmail.com>
2025-06-22 21:53:20 +0300

    Enable `global-auto-revert`

--- lisp/settings/general.el
+++ lisp/settings/general.el
@@ -2,6 +2,8 @@
 
 (ido-mode t)
 (save-place-mode 1)
+(global-auto-revert-mode 1)
 
 (setq gc-cons-threshold 50000000)
 (setq large-file-warning-threshold 100000000)
+(setq global-auto-revert-non-file-buffers t)
