dc60791ff008caf50a510a3a357eda519013637d
Martin Ivanov <xeartybg@gmail.com>
2025-06-29 06:03:52 +0300

    Fix a continuous error logging due to `doom-modeline` misconfiguration

--- plugins/doom-modeline.el
+++ plugins/doom-modeline.el
@@ -2,9 +2,10 @@
   :init
   (setq doom-modeline-hud t)
   (setq doom-modeline-buffer-modification-icon nil)
-  (setq doom-modeline-position-line-format nil)
-  (setq doom-modeline-position-column-format nil)
-  (setq doom-modeline-percent-position nil)
+  ;; (setq doom-modeline-position-line-format nil)
+  ;; (setq doom-modeline-position-column-format nil)
+  ;; (setq doom-modeline-position-column-line-format nil)
+  ;; (setq doom-modeline-percent-position nil)
   (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
   (setq doom-modeline-indent-info t)
   (setq doom-modeline-vcs-icon t)
